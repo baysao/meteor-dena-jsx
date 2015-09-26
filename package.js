21dbc0fffae7de89ac95d89f49ed0526f630655f
Vu Tran <baysao@gmail.com>
2015-09-27 01:24:16 +0700

    fix version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  summary: "JSX compiler",
-  version: "0.1.0-1",
+  summary: "JSXX compiler",
+  version: "0.1.0-2",
   name: "baysao:dena-jsx",
   git: "https://github.com/baysao/meteor-dena-jsx.git"
 });
 
-Npm.depends({
-  "jsx": "0.9.89"
-});
+// Npm.depends({
+//   "jsx": "0.9.89"
+// });
 
 Package.registerBuildPlugin({
   name: "compile-jsxx",
@@ -21,7 +21,7 @@ Package.registerBuildPlugin({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.1.0.2');
+  api.versionsFrom('METEOR@1.2');
   api.use('isobuild:compiler-plugin@1.0.0');
   api.export('JSX');
 });
