3f7b97bae4a1129dab37fd8e997d39843f6f6e55
Vu Tran <baysao@gmail.com>
2015-09-30 14:04:46 +0700

    fix error when compile

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "JSXX compiler",
-  version: "0.1.0-2",
+  version: "0.1.0-3",
   name: "baysao:dena-jsx",
   git: "https://github.com/baysao/meteor-dena-jsx.git"
 });
